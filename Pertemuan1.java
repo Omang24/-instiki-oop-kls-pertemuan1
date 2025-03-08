35f35b3a13e7d259163c78cd9a22e05de59d3339
omang uhuyy <squisd123@gmail.com>
2025-03-08 12:22:07 +0800

    Add files via upload

---
+++ Pertemuan1.java
@@ -0,0 +1,17 @@
+package pertemuan.pkg1;
+
+/**
+ *
+ * @author Komang Adi
+ * TGL: 8 Maret 2025
+ */
+public class Pertemuan1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("latihan bahasa java");
+    }
+}
